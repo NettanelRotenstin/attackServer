e26cc3c8badfca0f119fe17ff6c1e8a51e544060
Nettanel <nettanel1999@gmail.com>
2024-08-06 10:02:40 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace attackServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+C: \Users\INTERNET\.docker\attackServer\attackServer.sln
